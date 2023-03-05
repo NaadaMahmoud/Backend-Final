a10a15cf732cf28185e73b0966d0aa1483363478
Aly1294 <aly.elwardany94@gmail.com>
2023-03-04 23:11:34 -0800

    test to pull

--- backend/src/controllers/customOrderProposalsController.js
+++ backend/src/controllers/customOrderProposalsController.js
@@ -15,8 +15,44 @@ async function getAllProposalOfSpecificCustomOrder (id){
 
 async function post_new_proposal (id,proposals_data){
     console.log("post new proposal here in controller");
-    let new_proposal = await customOrdersProposalsModel.updateOne({_id:id},{$push:{proposals:proposals_data}})
-    return new_proposal;
+    // let new_proposal = await customOrdersProposalsModel.updateOne({_id:id},{$push:{proposals:proposals_data}})
+    let flag="";
+    await customOrderModel.findOne({_id:id})
+    
+    .then((data,error)=>{
+        
+        // console.log("aaa",data)
+        // console.log("isClosed = ",data.isclosed)
+        if(data.isclosed==false){
+            flag="Done";
+
+            // for (let i = 0; i < data.proposals.length; i++) {
+                // if(data.proposal[i].vendorID!=vendorID){
+                    data.proposals
+                    data.proposals.push(proposals_data)
+                    data.save();
+                    console.log("if = ",data.isclosed)
+                    return "Done"
+                // }
+                
+            // }
+            
+            }
+            else{
+            console.log("else = ",data.isclosed)
+
+                return "Not Done"
+            }
+        
+        
+    })
+
+        
+
+    return {
+        message:flag,
+        status:200,
+    }
  
 }
 
